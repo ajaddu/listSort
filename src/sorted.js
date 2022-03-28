662cb9171b2e767de924f4119f9e572f71d48b1e
AJ <aj@example.com>
2022-03-28 17:33:47 -0400

    Second passing test for 2 items reverse order array

--- src/sorted.js
+++ src/sorted.js
@@ -1,5 +1,24 @@
 function sorted(inputList){
     let list = inputList;
+    let len = inputList.length;
+
+    for(i=0; i<len; i++){
+        for(j=1; j<len; j++){
+            if(list[i] > list[j]){
+                console.log(list[i]);
+                console.log(list[j]);
+                // swap them
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+                break;
+            }
+        }
+
+    }
+
+    console.log(list);
+   
     return list;
 }
 
--- tests/stringsort.test.js
+++ tests/stringsort.test.js
@@ -4,3 +4,8 @@ test('Sort order for 1,2 is 1,2', ()=>{
     list = [1,2];
     expect(sorted(list)).toStrictEqual([1,2]);
 })
+
+test('Sort order for 2,1 is 1,2', ()=>{
+    list = [2,1];
+    expect(sorted(list)).toStrictEqual([1,2]);
+})
