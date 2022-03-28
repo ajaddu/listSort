67a976f4c1faf73258bc90f375bd40a17121d33d
AJ <aj@example.com>
2022-03-28 17:44:00 -0400

    Third passing test with bug fixes.

--- src/sorted.js
+++ src/sorted.js
@@ -2,17 +2,12 @@ function sorted(inputList){
     let list = inputList;
     let len = inputList.length;
 
-    for(i=0; i<len; i++){
-        for(j=1; j<len; j++){
-            if(list[i] > list[j]){
-                console.log(list[i]);
-                console.log(list[j]);
-                // swap them
-                temp = list[i];
-                list[i] = list[j];
-                list[j] = temp;
-                break;
-            }
+    for(i=0, j=1; i<len; i++, j++){
+        if(list[i] > list[j]){
+            // swap them
+            temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
         }
 
     }
--- tests/stringsort.test.js
+++ tests/stringsort.test.js
@@ -9,3 +9,8 @@ test('Sort order for 2,1 is 1,2', ()=>{
     list = [2,1];
     expect(sorted(list)).toStrictEqual([1,2]);
 })
+
+test('Sort order for 2,1,3,4,6,9,8 is 1,2,3,4,6,8,9', ()=>{
+    list = [2,1,3,4,6,9,8];
+    expect(sorted(list)).toStrictEqual([1,2,3,4,6,8,9]);
+})
